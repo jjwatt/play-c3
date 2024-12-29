8b1e499823ccab3ab1b3feb78f8d0eb9c26d7145
Jesse Wattenbarger <jwatt@broken.watch>
2024-12-29 09:36:20 -0500

    Add Square struct and use it

--- hello-caca.c
+++ hello-caca.c
@@ -5,7 +5,7 @@ struct Square {
     int h;
     int x;
     int y;
-}
+};
 
 int main(void)
 {
@@ -17,28 +17,24 @@ int main(void)
     cv = caca_get_canvas(dp);
     int w = caca_get_canvas_width(cv);
     int h = caca_get_canvas_height(cv);
-    struct Square p = { 20, 10, (w / 2) - 50, (h / 2) - 10 };
-    int square_w = 20;
-    int square_h = 10;
-    int square_x = (w / 2) - 50;
-    int square_y = (h / 2) - 10;
+    struct Square square = { 20, 10, (w / 2) - 50, (h / 2) - 10 };
     int dx = 1;
     int dy = 1;
     caca_set_color_ansi(cv, CACA_BLUE, CACA_BLACK);
-    caca_fill_box(cv, square_x, square_y, square_w, square_h, '#');
+    caca_fill_box(cv, square.x, square.y, square.w, square.h, '#');
     while (!caca_get_event(dp, CACA_EVENT_KEY_PRESS, &ev, 0)) {
 	caca_clear_canvas(cv);
-	square_x += dx;
-	square_y += dy;
-	if (square_x >= w - square_w || square_x <= 0) {
+	square.x += dx;
+	square.y += dy;
+	if (square.x >= w - square.w || square.x <= 0) {
 	    caca_set_color_ansi(cv, caca_rand(1, 16), CACA_BLACK);
 	    dx *= -1;
 	}
-	if (square_y >= h - square_h || square_y <= 0) {
+	if (square.y >= h - square.h || square.y <= 0) {
 	    caca_set_color_ansi(cv, caca_rand(1, 16), CACA_BLACK);
 	    dy *= -1;
 	}
-	caca_fill_box(cv, square_x, square_y, square_w, square_h, '#');
+	caca_fill_box(cv, square.x, square.y, square.w, square.h, '#');
 	caca_refresh_display(dp);
 	usleep(80000);
     }
