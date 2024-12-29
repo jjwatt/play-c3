b62c2ee860a3e67541ac162ee8d8d1d8ddfeae45
Jesse Wattenbarger <jwatt@broken.watch>
2024-12-29 07:12:34 -0500

    Refactoring to structure in process

--- hello-caca.c
+++ hello-caca.c
@@ -1,5 +1,11 @@
-#include <stdio.h>
 #include <caca.h>
+
+struct Square {
+    int w;
+    int h;
+    int x;
+    int y;
+}
 
 int main(void)
 {
@@ -9,9 +15,9 @@ int main(void)
     dp = caca_create_display(NULL);
     if (!dp) return 1;
     cv = caca_get_canvas(dp);
-
     int w = caca_get_canvas_width(cv);
     int h = caca_get_canvas_height(cv);
+    struct Square p = { 20, 10, (w / 2) - 50, (h / 2) - 10 };
     int square_w = 20;
     int square_h = 10;
     int square_x = (w / 2) - 50;
@@ -20,14 +26,10 @@ int main(void)
     int dy = 1;
     caca_set_color_ansi(cv, CACA_BLUE, CACA_BLACK);
     caca_fill_box(cv, square_x, square_y, square_w, square_h, '#');
-
-    while (!caca_get_event(dp, CACA_EVENT_KEY_PRESS, NULL, 0)) {
+    while (!caca_get_event(dp, CACA_EVENT_KEY_PRESS, &ev, 0)) {
 	caca_clear_canvas(cv);
-	/* caca_set_color_ansi(cv, CACA_BLUE, CACA_BLACK); */
-
 	square_x += dx;
 	square_y += dy;
-
 	if (square_x >= w - square_w || square_x <= 0) {
 	    caca_set_color_ansi(cv, caca_rand(1, 16), CACA_BLACK);
 	    dx *= -1;
@@ -37,7 +39,6 @@ int main(void)
 	    dy *= -1;
 	}
 	caca_fill_box(cv, square_x, square_y, square_w, square_h, '#');
-
 	caca_refresh_display(dp);
 	usleep(80000);
     }
