f9cfa9db6bb427182220d74332ad7142352f0c82
Jesse Wattenbarger <jwatt@broken.watch>
2024-12-31 23:47:49 -0500

    I think this plasma is workingish
    It's just not very good

--- bad-plasma-caca.c
+++ bad-plasma-caca.c
@@ -6,7 +6,7 @@
 
 // Simple noise function (you can use more sophisticated ones like Perlin noise)
 double noise(double x, double y) {
-    return sin(x) + cos(y); 
+    return sin(y) + cos(x); 
 }
 
 int main() {
@@ -18,7 +18,7 @@ int main() {
     int width = caca_get_canvas_width(cv);
     int height = caca_get_canvas_height(cv);
     char frame[height][width];
-    while (1) { // Infinite loop for continuous animation
+    while (!caca_get_event(dp, CACA_EVENT_KEY_PRESS, NULL, 0)) {
         double x_offset = sin(clock() / 1000.0); // Simple animation for x-offset
         double y_offset = cos(clock() / 1500.0); // Simple animation for y-offset
 
@@ -26,21 +26,21 @@ int main() {
             for (int x = 0; x < width; x++) {
                 double n = noise(x + x_offset, y + y_offset); 
                 n = (n + 1.0) * 0.5; // Normalize to 0.0 - 1.0
-                int intensity = n * 7; // Map to character intensity (0-7)
-                frame[y][x] = " .:-=+*#%@"[intensity]; 
+		/* Map noise to color */
+		int red = (int)(n * 16);
+		int green = (int)((1 - n) * 16);
+		int blue = (int)((n * 0.5) * 16);
+
+		caca_set_color_ansi(cv, red, CACA_BLACK);
+		caca_put_char(cv, x, y, '#');
             }
         }
-
+	caca_refresh_display(dp);
+	usleep(10000);
         // Clear the screen (system-specific)
         caca_clear_canvas(cv);
-
-        // Print the frame
-        for (int y = 0; y < height; y++) {
-            for (int x = 0; x < width; x++) {
-		caca_put_char(cv, x, y, fame[y][x])
-                /* printf("%c", frame[y][x]); */
-            }
-        }
     }
+    caca_free_canvas(cv);
+    caca_free_display(dp);
     return 0;
 }
