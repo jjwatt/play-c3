aebcf54ce4185eed3e6cd5bde62ffd05db4d1527
Jesse Wattenbarger <jwatt@broken.watch>
2025-01-09 11:59:17 -0500

    Better make rules and image loading updates

--- c/load_ansi_image.c
+++ c/load_ansi_image.c
@@ -11,18 +11,19 @@ int main() {
 
   // Load the file
   caca_canvas_t *imported_cv = caca_create_canvas(0, 0);
-  ssize_t size = caca_import_canvas_from_file(imported_cv, "reply.txt", "ansi");
+  ssize_t size = caca_import_canvas_from_file(imported_cv, "reply.caca", "caca");
   if (!imported_cv) {
     fprintf(stderr, "Error importing ANSI file\n");
     caca_free_display(dp);
     caca_free_canvas(cv);
     return 1;
   }
+  int cv_width = caca_get_canvas_width(cv);
+  int cv_height = caca_get_canvas_height(cv);
+  int img_width = caca_get_canvas_width(imported_cv);
+  int img_height = caca_get_canvas_height(imported_cv);
 
-  int width = caca_get_canvas_width(imported_cv);
-  int height = caca_get_canvas_height(imported_cv);
-
-  caca_blit(cv, 0, 0, imported_cv, NULL);
+  caca_blit(cv, (cv_width/2) - img_width/2, cv_height/2 - img_height/2, imported_cv, NULL);
 
   caca_refresh_display(dp);
   caca_get_event(dp, CACA_EVENT_KEY_PRESS, NULL, -1);
