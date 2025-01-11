0de6a55d23063f182e37ffb45939a7249f440379
Jesse Wattenbarger <jwatt@broken.watch>
2025-01-11 11:56:11 -0500

    updates to load_ansi_image

--- c/load_ansi_image.c
+++ c/load_ansi_image.c
@@ -5,6 +5,7 @@
 int main() {
   caca_canvas_t *cv;
   caca_display_t *dp;
+  caca_event_t ev;
   dp = caca_create_display(NULL);
   if (!dp) return 1;
   cv = caca_get_canvas(dp);
@@ -22,16 +23,16 @@ int main() {
   int cv_height = caca_get_canvas_height(cv);
   int img_width = caca_get_canvas_width(imported_cv);
   int img_height = caca_get_canvas_height(imported_cv);
+  fprintf(stderr, "img_width: %d\n", img_width);
+  fprintf(stderr, "img_height: %d\n", img_height);
 
-  caca_blit(cv, (cv_width/2) - img_width/2, cv_height/2 - img_height/2, imported_cv, NULL);
-
-  caca_refresh_display(dp);
-  caca_get_event(dp, CACA_EVENT_KEY_PRESS, NULL, -1);
-
+  while (!caca_get_event(dp, CACA_EVENT_KEY_PRESS | CACA_EVENT_QUIT, &ev, 0)) {
+    caca_blit(cv, (cv_width/2) - img_width/2, cv_height/2 - img_height/2, imported_cv, NULL);
+    caca_refresh_display(dp);
+  } 
   caca_free_canvas(imported_cv); // Free the imported canvas
   caca_free_canvas(cv);
   caca_free_display(dp);
 
   return 0;
-
 }
