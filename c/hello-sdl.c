3a8123cdf71fee1ade2f7cd78968707df5ddaaeb
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2025-01-14 15:18:24 -0500

    Maybe finish sdl2 hello world

--- c/hello-sdl.c
+++ c/hello-sdl.c
@@ -3,15 +3,27 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
-#define IMAGE_WIDTH 256
-#define IMAGE_HEIGHT 256
-
 int main(int argc, char* argv[])
 {
     // SDL Initialization
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 	SDL_Log("SDL_Init Error: %s\n", SDL_GetError());
 	return 1;
     }
-    
+
+    // Window creation and position in the center of the screen
+    SDL_Window* window = SDL_CreateWindow("Hello World SDL", SDL_WINDOWPOS_CENTERED,
+					  SDL_WINDOWPOS_CENTERED,
+					  WINDOW_WIDTH,
+					  WINDOW_HEIGHT,
+					  SDL_WINDOW_SHOWN);
+
+    SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
+    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(2000);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
 }
+
