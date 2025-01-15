75fd55dee5299e4dac24deec6f6a44f988f7ccc5
Jesse Wattenbarger <jwattenbarger@spencerhealthsolutions.com>
2025-01-14 20:12:33 -0500

    Add Makefile and fix sdl2 hello world
    Now it at least leaves up a window until you close it. I want to
    actually draw a square in it or something, though.

--- c/hello-sdl.c
+++ c/hello-sdl.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+#include <stdbool.h>
 
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
@@ -12,18 +13,29 @@ int main(int argc, char* argv[])
     }
 
     // Window creation and position in the center of the screen
-    SDL_Window* window = SDL_CreateWindow("Hello World SDL", SDL_WINDOWPOS_CENTERED,
-					  SDL_WINDOWPOS_CENTERED,
+    SDL_Window* window = SDL_CreateWindow("Hello World SDL", SDL_WINDOWPOS_UNDEFINED,
+					  SDL_WINDOWPOS_UNDEFINED,
 					  WINDOW_WIDTH,
 					  WINDOW_HEIGHT,
 					  SDL_WINDOW_SHOWN);
 
     SDL_Surface* screenSurface = SDL_GetWindowSurface(window);
-    SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(2000);
+    SDL_Event e;
+
+    bool quit = false;
+
+    while (!quit) {
+	while (SDL_PollEvent(&e) != 0) {
+	    if (e.type == SDL_QUIT) {
+		quit = true;
+	    }
+	}
+
+	SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+	SDL_UpdateWindowSurface(window);
+    }
+
     SDL_DestroyWindow(window);
     SDL_Quit();
     return 0;
 }
-
